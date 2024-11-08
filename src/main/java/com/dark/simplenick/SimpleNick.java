254bf36576204ef9410353adc800aa481f9fc33d
Dark <tristan.marty240112@gmail.com>
2024-11-08 21:11:37 +0100

    Update SimpleNick.java

--- src/main/java/com/dark/simplenick/SimpleNick.java
+++ src/main/java/com/dark/simplenick/SimpleNick.java
@@ -1,22 +1,47 @@
 package com.dark.simplenick;
 
-import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleNick extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Save default configuration if it doesn’t exist
-        saveDefaultConfig();
-
-        // Register the /nick command executor
-        this.getCommand("nick").setExecutor(new NickCommand(this));
-        getLogger().info(ChatColor.GREEN + "SimpleNick plugin has been enabled.");
+        getLogger().info("SimpleNick has been enabled!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info(ChatColor.RED + "SimpleNick plugin has been disabled.");
+        getLogger().info("SimpleNick has been disabled!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("This command can only be used by players.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (command.getName().equalsIgnoreCase("nick")) {
+            if (args.length == 0) {
+                player.sendMessage("Usage: /nick <nickname> or /nick reset");
+                return true;
+            }
+
+            if (args[0].equalsIgnoreCase("reset")) {
+                player.setDisplayName(player.getName());
+                player.sendMessage("Nickname reset to your original name.");
+            } else {
+                String nickname = String.join(" ", args);
+                player.setDisplayName(nickname);
+                player.sendMessage("Your nickname has been changed to: " + nickname);
+            }
+            return true;
+        }
+        return false;
     }
 }
